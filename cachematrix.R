fe5987f6ad7febcc11a632e230274285cd6aac24
sandeep <sandeep_k17@yahoo.com>
2014-08-24 11:57:57 +0530

    Update cachematrix.R
    Lexical Scoping Assignment2

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,55 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+## Initialising the inverse property
+  i <- NULL
+##Method for setting matrix
+  set <- function( matrix ) {
+    m <<- matrix
+    i <<- NULL
+  }
+## getting value of the matrix
+  get <- function() {
+## Return the matrix
+    m 
 
-}
-
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
+## Method for getting the inverse of the matrix
+    setInverse <- function(inverse) {
+      i <<- inverse
+    }
+    
+## Method for getting the inverse of the matrix
+    getInverse <- function() {
+        }
+        ## Return the inverse property
+        i
+        }
+   
+        ## Return a list of the methods
+        list(set = set, get = get,
+        setInverse = setInverse,
+        getInverse = getInverse)
+        }
+        ## Write a short comment describing this function
+        cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-}
+        m <- x$getInverse()
+
+        ## Just return the inverse if its already set
+        if( !is.null(m) ) {
+        message("getting cached data")
+        return(m)
+         }
+
+        ## Get the matrix from our object
+        data <- x$get()
+        
+        ## Calculate the inverse using matrix multiplication
+        m <- solve(data) %*% data
+
+        ## Set the inverse to the object
+        x$setInverse(m)
+        
+        ## Return the matrix
+        m
+        }
